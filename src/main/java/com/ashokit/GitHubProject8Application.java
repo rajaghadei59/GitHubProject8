8f166a6c692b77b1ab5d9c735279598e91a6144f
rajaghadei59 <ganeshghadei2@gmail.com>
2020-06-13 18:42:38 +0530

    First Commit....

---
+++ src/main/java/com/ashokit/GitHubProject8Application.java
@@ -0,0 +1,13 @@
+package com.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProject8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubProject8Application.class, args);
+	}
+
+}
