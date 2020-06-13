ba14523bc099c4def2336259a26ebdd6f1d7f0e6
rajaghadei59 <ganeshghadei2@gmail.com>
2020-06-13 20:12:21 +0530

    Added Second commit in start class....

--- src/main/java/com/ashokit/GitHubProject8Application.java
+++ src/main/java/com/ashokit/GitHubProject8Application.java
@@ -2,12 +2,13 @@ package com.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitHubProject8Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GitHubProject8Application.class, args);
+		ConfigurableApplicationContext ctx= SpringApplication.run(GitHubProject8Application.class, args);
 	}
 
 }
